a86a1678d40732befc80ee3fb50e9b40cdca1306
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-14 12:04:40 -0500

    final test

--- SimpleTableEntry/index.js
+++ SimpleTableEntry/index.js
@@ -1,1 +1,21 @@
-$("header").css("color ", "red");
+const makingTable = () => {
+  const firstRow = $("#first-row-1").val();
+  const secondRow1 = $("#second-row-1").val();
+  const secondRow2 = $("#second-row-2").val();
+
+  $(".actual-table").append(
+    "<tr class=items> <td>" +
+      secondRow2 +
+      " </td> <td> " +
+      firstRow +
+      "</td> <td> " +
+      secondRow1 +
+      "</td> </tr>"
+  );
+  $(".items").click(function () {
+    $(this).remove();
+  });
+  $("#first-row-1").val("");
+  $("#second-row-1").val("");
+  $("#second-row-2").val("");
+};
