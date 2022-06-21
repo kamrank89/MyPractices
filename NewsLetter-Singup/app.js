c3e09486e9d09700d64c81848f0f1bdacf7bbd04
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-21 11:49:45 -0500

    files added

--- NewsLetter-Singup/app.js
+++ NewsLetter-Singup/app.js
@@ -4,4 +4,17 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static(__dirname + "/"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/singup.html");
+});
+app.post("/", function (req, res) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+});
+
 app.listen(3000, () => console.log("app is running at port 3000"));
