2a59259017dc5c4229eefcb2378d3addea1ed714
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-21 13:15:27 -0500

    compelete

--- NewsLetter-Singup/app.js
+++ NewsLetter-Singup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -14,7 +15,36 @@ app.post("/", function (req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us8.api.mailchimp.com/3.0/lists/e56bc3fcd4";
+  options = {
+    method: "POST",
+    auth: "kamran11:cb1d239a8b2fc9de7abd22e7e7a24560-us8",
+  };
+  const requestchimp = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  requestchimp.write(jsonData);
+  requestchimp.end;
 });
 
 app.listen(3000, () => console.log("app is running at port 3000"));
+
+/* API key */
+// cb1d239a8b2fc9de7abd22e7e7a24560-us8
+/* Audience ID */
+// e56bc3fcd4
