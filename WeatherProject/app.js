71f3c7741c06dd9af60b8188ddaedb1ec7e8adb0
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-21 10:00:39 -0500

    add html

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -3,19 +3,30 @@ const app = express();
 const https = require("https");
 
 app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=50c8eac93afe4cd51672ebfa0aa65aa1&units=metric";
-  https.get(url, function (response) {
-    console.log(response.statusCode);
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      console.log(weatherData.main.feels_like);
-      console.log(weatherData.weather[0].description);
-      res.send(" <div><h1> temp is : " + weatherData.main.temp + " celcius </h1>
-      <h2> dexription : " + weatherData.weather[0].description + "</h>" "
-      </div>"
-      );
-    });
-  });
+  res.sendFile(__dirname + "/index.html");
 });
+app.post("/", function (req, res) {
+  console.log("post recieved");
+});
+
 app.listen(3000, () => console.log("server is running at port 3000!"));
+// const query = "Austin";
+//   const url =
+//     "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     query +
+//     "&appid=50c8eac93afe4cd51672ebfa0aa65aa1&units=metric";
+//   https.get(url, function (response) {
+//     console.log(response.statusCode);
+//     response.on("data", function (data) {
+//       const weatherData = JSON.parse(data);
+//       console.log(weatherData.main.feels_like);
+//       console.log(weatherData.weather[0].description);
+//       res.send(
+//         " <h1> temp in " +
+//           query +
+//           " is : " +
+//           weatherData.main.temp +
+//           " celcius </h1>"
+//       );
+//     });
+//   });
