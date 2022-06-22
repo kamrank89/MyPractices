cd06c3a86f1d3e1262885e9c179392d22d8ce983
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-22 11:42:43 -0500

    list ejs add plus ejs package install

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,15 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("yay it's the weekend");
+  if (currentDay === 0) {
+    day = "sunday";
+  } else if (currentDay === 1) {
+    day = "monday";
+  } else if (currentDay === 2) {
+    day = "tuesday";
+  } else if (currentDay === 3) {
+    day = "wednesday";
+  } else if (currentDay === 4) {
+    day = "thursday";
+  } else if (currentDay === 5) {
+    day = "friday";
   } else {
-    res.send("boo i have to work!");
+    day = "saturday";
   }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => console.log("server running on port 3000"));
