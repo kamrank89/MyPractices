f70c5b44dd17c9dbd602fa1dbd724d4852c5507e
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-24 12:22:38 -0500

    todolist compelete

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var items = [];
+let items = [];
+let works = [];
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 app.get("/", function (req, res) {
   var today = new Date();
   var options = {
@@ -17,13 +18,22 @@ app.get("/", function (req, res) {
     month: "long",
   };
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItem: works });
+});
 app.post("/", function (req, res) {
   var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work List") {
+    works.push(items);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+
+    res.redirect("/");
+  }
 });
 
 app.listen(3000, () => console.log("server running on port 3000"));
