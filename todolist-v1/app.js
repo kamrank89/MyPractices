23b3d701877d85d060388b07a101203d815fab7a
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-24 11:12:19 -0500

    some functionality add

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var items = [];
+
 const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItem: items });
+});
 
-  if (currentDay === 0) {
-    day = "sunday";
-  } else if (currentDay === 1) {
-    day = "monday";
-  } else if (currentDay === 2) {
-    day = "tuesday";
-  } else if (currentDay === 3) {
-    day = "wednesday";
-  } else if (currentDay === 4) {
-    day = "thursday";
-  } else if (currentDay === 5) {
-    day = "friday";
-  } else {
-    day = "saturday";
-  }
-  res.render("list", { kindOfDay: day });
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("server running on port 3000"));
