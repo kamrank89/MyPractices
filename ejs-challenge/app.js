7f01499634bf5c12b02014727ec87312e30e4cfa
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-25 11:30:35 -0500

    app complete without database

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+
+let posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,7 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
@@ -36,8 +42,18 @@ app.post("/compose", function (req, res) {
     title: req.body.titleInput,
     content: req.body.contentinput,
   };
+  posts.push(post);
+  res.redirect("/");
 });
 
+app.get("/posts/:postName", function (req, res) {
+  const test = _.lowerCase(req.params.postName);
+  posts.forEach(function (post) {
+    if (test === post.title) {
+      res.render("post", { someValue: post.title, someContent: post.content });
+    }
+  });
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
