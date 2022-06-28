9e95469a4bb7500dcdf1a406b1809a9992d3cc57
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-28 11:50:48 -0500

    integrated a database tothe app

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 let items = [];
 let works = [];
@@ -10,30 +11,61 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+const item2 = new Item({
+  name: "hit the + to add item.",
+});
+const item3 = new Item({
+  name: "<-- hit this to delete item.",
+});
+
+const defualtItems = [item1, item2, item3];
+
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { listTitle: day, newListItem: items });
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defualtItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("added to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItem: foundItems });
+    }
+  });
 });
 
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItem: works });
 });
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
-  if (req.body.list === "Work List") {
-    works.push(items);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
 });
 
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkBox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("deleted Successfully");
+    }
+  });
+  res.redirect("/");
+});
 app.listen(3000, () => console.log("server running on port 3000"));
