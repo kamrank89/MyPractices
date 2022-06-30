47f127c6262a6e2ab4db2bf9aeef7d8cd522fe25
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-30 11:47:03 -0500

    post method add

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -33,6 +33,12 @@ app.get("/articles", function (req, res) {
     }
   });
 });
+
+app.post("/articles", function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.content);
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
