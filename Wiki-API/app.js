cffc680c56b577ccd11df02b462db84860459957
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-30 11:21:26 -0500

    add new practice file

---
+++ Wiki-API/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
+
+//TODO
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      console.log(foundArticles);
+      res.send(foundArticles);
+    } else {
+      console.log(err);
+    }
+  });
+});
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
