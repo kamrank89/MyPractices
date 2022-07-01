2931dc8e75bfaade07cdf7591ebc81ce26134fc2
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-07-01 10:17:56 -0500

    add encryption to username and password

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,9 +1,10 @@
-//jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
+const encrypt = require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -11,10 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 /* Mongoose Configurations */
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 const User = new mongoose.model("User", userSchema);
 
 /* Home Page */
