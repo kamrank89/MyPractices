d5f488d945f855c7914a96767517092f9f39330b
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-07-16 09:33:50 -0500

    some changes

--- Data-structures/Searching-Algorithms/binary-search.js
+++ Data-structures/Searching-Algorithms/binary-search.js
@@ -1,19 +1,16 @@
 function binary(arr, num) {
   let left = 0;
-  let right = arr.length;
-  while (left < right) {
-    let middle = Math.floor((left + right) / 2);
-    if (middle === num) {
-      return arr.indexOf(middle);
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+  while (arr[middle] !== num) {
+    middle = Math.floor((left + right) / 2);
+    if (num < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
     }
-    if (middle < num) {
-      left = middle;
-    }
-    if (middle > num) {
-      right = middle;
-    }
-    return -1;
   }
+  return middle;
 }
 
-console.log(binary([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
+console.log(binary([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
