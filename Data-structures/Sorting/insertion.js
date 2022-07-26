ff5f2fdebee964321c0cc9a247c20f671eab48ad
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-07-26 12:47:47 -0500

    insertion sort add

--- Data-structures/Sorting/insertion.js
+++ Data-structures/Sorting/insertion.js
@@ -1,16 +1,25 @@
-function insertionSorting(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let smallestElement = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[smallestElement]) {
-        smallestElement = j;
+function swap(arr, num1, num2) {
+  var temp = arr[num1];
+  arr[num1] = arr[num2];
+  arr[num2] = arr[temp];
+}
+
+function inserionSort(arr) {
+  let start = arr[1];
+  if (arr[1] < arr[0]) {
+    swap(arr, arr[1], arr[0]);
+  }
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      for (let j = arr[i]; j > 0; j--) {
+        if (arr[i] < arr[j]) {
+          swap(arr[i], arr[j]);
+        }
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[smallestElement];
-    arr[smallestElement] = temp;
   }
   return arr;
 }
 
-console.log(insertionSorting([9, 3, 4, 6, 5]));
+console.log(swap([2, 4], 2, 4));
+// console.log(inserionSort([3, 2, 7, 15, 8, 10]));
