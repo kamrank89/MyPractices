d6fcb84203b241cac56041d855ab0437bab0bf23
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-07-27 11:17:57 -0500

    merge sort compelete

--- Data-structures/Sorting/merge.js
+++ Data-structures/Sorting/merge.js
@@ -1,11 +1,8 @@
 function merge(arr1, arr2) {
   let mergedArray = [];
-  let arr1Length = arr1.length;
-  let arr2Length = arr2.length;
   let i = Number(0);
   let j = Number(0);
-
-  while (i !== arr1Length && j !== arr2Length) {
+  while (i !== arr1.length && j !== arr2.length) {
     if (arr1[i] <= arr2[j]) {
       mergedArray.push(arr1[i]);
       i++;
@@ -25,4 +22,12 @@ function merge(arr1, arr2) {
   return mergedArray;
 }
 
-console.log(merge([], [1, 3]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([6, 7, 8, 1, 3, 4, 5]));
